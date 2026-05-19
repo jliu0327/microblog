27dac0ae5c60f6c6edb39b7902af5de598b2cd23
jliu0327 <justinliu90@gmail.com>
2026-05-18 23:44:36 -0400

    Added User login validation

--- app/models.py
+++ app/models.py
@@ -1,10 +1,9 @@
-from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
